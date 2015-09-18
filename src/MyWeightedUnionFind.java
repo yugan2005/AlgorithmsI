abc7ae417aa63dca29bf145d4a69285331437e8e
yugan2005 <yugan2005@gmail.com>
2015-09-18 15:20:36 -0700

    Solution for Quiz1

---
+++ src/MyWeightedUnionFind.java
@@ -0,0 +1,128 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+
+public class MyWeightedUnionFind {
+
+	
+    private int[] parent;   // parent[i] = parent of i
+    private int[] size;     // size[i] = number of sites in subtree rooted at i
+    private int count;      // number of components
+
+    /**
+     * Initializes an empty union-find data structure with <tt>N</tt> sites
+     * <tt>0</tt> through <tt>N-1</tt>. Each site is initially in its own 
+     * component.
+     *
+     * @param  N the number of sites
+     * @throws IllegalArgumentException if <tt>N &lt; 0</tt>
+     */
+    public MyWeightedUnionFind(int N) {
+        count = N;
+        parent = new int[N];
+        size = new int[N];
+        for (int i = 0; i < N; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    /**
+     * Returns the number of components.
+     *
+     * @return the number of components (between <tt>1</tt> and <tt>N</tt>)
+     */
+    public int count() {
+        return count;
+    }
+  
+    /**
+     * Returns the component identifier for the component containing site <tt>p</tt>.
+     *
+     * @param  p the integer representing one object
+     * @return the component identifier for the component containing site <tt>p</tt>
+     * @throws IndexOutOfBoundsException unless <tt>0 &le; p &lt; N</tt>
+     */
+    public int find(int p) {
+        validate(p);
+        while (p != parent[p])
+            p = parent[p];
+        return p;
+    }
+
+    // validate that p is a valid index
+    private void validate(int p) {
+        int N = parent.length;
+        if (p < 0 || p >= N) {
+            throw new IndexOutOfBoundsException("index " + p + " is not between 0 and " + (N-1));  
+        }
+    }
+
+    /**
+     * Returns true if the the two sites are in the same component.
+     *
+     * @param  p the integer representing one site
+     * @param  q the integer representing the other site
+     * @return <tt>true</tt> if the two sites <tt>p</tt> and <tt>q</tt> are in the same component;
+     *         <tt>false</tt> otherwise
+     * @throws IndexOutOfBoundsException unless
+     *         both <tt>0 &le; p &lt; N</tt> and <tt>0 &le; q &lt; N</tt>
+     */
+    public boolean connected(int p, int q) {
+        return find(p) == find(q);
+    }
+
+    /**
+     * Merges the component containing site <tt>p</tt> with the 
+     * the component containing site <tt>q</tt>.
+     *
+     * @param  p the integer representing one site
+     * @param  q the integer representing the other site
+     * @throws IndexOutOfBoundsException unless
+     *         both <tt>0 &le; p &lt; N</tt> and <tt>0 &le; q &lt; N</tt>
+     */
+    public void union(int p, int q) {
+        int rootP = find(p);
+        int rootQ = find(q);
+        if (rootP == rootQ) return;
+
+        // make smaller root point to larger one
+        if (size[rootP] < size[rootQ]) {
+            parent[rootP] = rootQ;
+            size[rootQ] += size[rootP];
+        }
+        else {
+            parent[rootQ] = rootP;
+            size[rootP] += size[rootQ];
+        }
+        count--;
+    }
+    
+    public int[] parent(){
+    	return parent;
+    }
+
+
+    /**
+     * Reads in a sequence of pairs of integers (between 0 and N-1) from standard input, 
+     * where each integer represents some object;
+     * if the sites are in different components, merge the two components
+     * and print the pair to standard output.
+     */
+    public static void main(String[] args) {
+        int N = StdIn.readInt();
+        WeightedQuickUnionUF uf = new WeightedQuickUnionUF(N);
+        while (!StdIn.isEmpty()) {
+            int p = StdIn.readInt();
+            int q = StdIn.readInt();
+            if (uf.connected(p, q)) continue;
+            uf.union(p, q);
+            StdOut.println(p + " " + q);
+        }
+        StdOut.println(uf.count() + " components");
+    }
+
+
+
+}
---
+++ src/Quiz1.java
@@ -0,0 +1,47 @@
+import edu.princeton.cs.algs4.*;
+
+public class Quiz1 {
+	
+
+	public static void main(String[] args) {
+		
+		// Question1
+		QuickFindUF myQF = new QuickFindUF(10);
+		
+		myQF.union(7,9);
+		myQF.union(9, 6);
+		myQF.union(0, 8);
+		myQF.union(1, 5);
+		myQF.union(7, 0);
+		myQF.union(4, 5);
+		
+		String question1="";
+		for (int i=0; i<10; i++){
+			question1 += myQF.find(i)+" ";
+		}
+		System.out.println(question1);
+		
+		// Question2
+		MyWeightedUnionFind myWQU = new MyWeightedUnionFind(10);
+		
+		myWQU.union(3, 6);
+		myWQU.union(8, 4);
+		myWQU.union(5, 2);
+		myWQU.union(4, 0);
+		myWQU.union(9, 1);
+		myWQU.union(4, 2);
+		myWQU.union(3, 7);
+		myWQU.union(1, 7);
+		myWQU.union(8, 7);
+		
+		String question2 = "";
+
+		for (int i : myWQU.parent()) question2 += i+" ";
+		System.out.println(question2);
+		
+		
+		
+
+	}
+
+}
