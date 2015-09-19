d2cf12923d4f65fb8ca5489111d53119f2758861
yugan2005 <yugan2005@gmail.com>
2015-09-19 10:14:45 -0700

    Week1 Completed!

--- src/Percolation.java
+++ src/Percolation.java
@@ -1,80 +1,118 @@
 import edu.princeton.cs.algs4.*;
 
 public class Percolation {
-	private WeightedQuickUnionUF grid;
+	private WeightedQuickUnionUF percolationGrid;
+	private WeightedQuickUnionUF fullGrid;
 	private int n;
 	private boolean[] open;
-	
-	public Percolation(int N){
-		if (N<=0) throw new IllegalArgumentException();
-		this.n=N;
-		open = new boolean[n*n+2];
-		open[0]=true;
-		open[N*N+1]=true;
-		grid = new WeightedQuickUnionUF(N*N+2);
-		for (int i=1; i<=N; i++){
-			grid.union(0, i);
-			grid.union(N*(N-1)+i, N*N+1);
-		}
-	}
-	
-	public void open(int i, int j){
-		// open site (row i, column j) if it is not open already
-		if (i<1 || i>n || j<1 || j>n) throw new IllegalArgumentException();
-		if (isOpen(i,j)) return;
-		open[i*j]=true;
-		int[] neighbor = getNeighor(i, j);
-		for (int k:neighbor) {
-			if (open[k]) grid.connected(i*j, k);	
-		}
-	}
-	
-   private int[] getNeighor(int i, int j) {
-	   int[] neighbor = new int[4];
-	   int idx=0;
-	   if (i!=1) {
-		   neighbor[idx] = (i-1)*j;
-		   idx++;
-	   }
-	   if (j!=1){
-		   neighbor[idx] = (j-1)*i;
-		   idx++;
-	   }
-	   if (i!=n){
-		   neighbor[idx] = (i+1)*j;
-		   idx++;
-	   }
-	   if (j!=n){
-		   neighbor[idx] = (j+1)*i;
-		   idx++;
-	   }
-	   
-	   int[] result = new int[idx];
-	   for (int k=0; k<idx; k++){
-		   result[k]=neighbor[k];
-	   }	   
-	   return result;
+
+	public Percolation(int N) {
+		if (N <= 0)
+			throw new IllegalArgumentException();
+		this.n = N;
+		open = new boolean[n * n + 2];
+		open[0] = true;
+		open[n * n + 1] = true;
+		percolationGrid = new WeightedQuickUnionUF(n * n + 2);
+		fullGrid = new WeightedQuickUnionUF(n * n + 1);
 	}
 
-   public boolean isOpen(int i, int j) {
-	   // is site (row i, column j) open?
-		if (i<1 || i>n || j<1 || j>n) throw new IllegalArgumentException();
-		return open[i*j];
-   }
-   
-   
-   public boolean isFull(int i, int j){
-	   // is site (row i, column j) full?
-	   if (i<1 || i>n || j<1 || j>n) throw new IllegalArgumentException();
-	   return grid.connected(0, i*j);
-   }
-   
-   public boolean percolates(){
-	   return grid.connected(0, n*n+1);
-   }
+	public void open(int i, int j) {
+		// open site (row i, column j) if it is not open already
+		if (i < 1 || i > n || j < 1 || j > n)
+			throw new IndexOutOfBoundsException();
+		if (isOpen(i, j))
+			return;
+		open[(i - 1) * n + j] = true;
+		int[] neighbor = getNeighor(i, j);
+		for (int k : neighbor) {
+			if (open[k]) {
+				percolationGrid.union((i - 1) * n + j, k);
+				if (k != n * n + 1)
+					fullGrid.union((i - 1) * n + j, k);
+			}
+		}
+
+	}
+
+	private int[] getNeighor(int i, int j) {
+		int[] neighbor = new int[4];
+		int idx = 0;
+		if (i != 1) {
+			neighbor[idx] = (i - 2) * n + j;
+			idx++;
+		} else {
+			neighbor[idx] = 0;
+			idx++;
+		}
+
+		if (j != 1) {
+			neighbor[idx] = (i - 1) * n + j - 1;
+			idx++;
+		}
+
+		if (i != n) {
+			neighbor[idx] = i * n + j;
+			idx++;
+		} else {
+			neighbor[idx] = n * n + 1;
+			idx++;
+		}
+
+		if (j != n) {
+			neighbor[idx] = (i - 1) * n + j + 1;
+			idx++;
+		}
+
+		int[] result = new int[idx];
+		for (int k = 0; k < idx; k++) {
+			result[k] = neighbor[k];
+		}
+		return result;
+	}
+
+	public boolean isOpen(int i, int j) {
+		// is site (row i, column j) open?
+		if (i < 1 || i > n || j < 1 || j > n)
+			throw new IndexOutOfBoundsException();
+		return open[(i - 1) * n + j];
+	}
+
+	public boolean isFull(int i, int j) {
+		// is site (row i, column j) full?
+		if (i < 1 || i > n || j < 1 || j > n)
+			throw new IndexOutOfBoundsException();
+		return (fullGrid.connected(0, (i - 1) * n + j));
+	}
+
+	public boolean percolates() {
+
+		return percolationGrid.connected(0, n * n + 1);
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		int n = 4;
+
+		Percolation test = new Percolation(n);
+
+		int[] openSequence = new int[n * n];
+
+		for (int i = 0; i < n * n; i++) {
+			openSequence[i] = i + 1;
+		}
+
+		StdRandom.shuffle(openSequence);
+
+		for (int j = 0; j < n * n; j++) {
+			int row = (openSequence[j] - 1) / n + 1;
+			int col = openSequence[j] - (row - 1) * n;
+			test.open(row, col);
+			if (test.percolates()) {
+				System.out.println(j);
+				break;
+			}
+		}
 	}
 
 }
--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -1,75 +1,68 @@
 import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
 	private int t, n;
-	private double[] threshold;
-	
-	public PercolationStats(int N, int T){
+	private double mean, stddev;
+
+	public PercolationStats(int N, int T) {
 		// perform T independent experiments on an N-by-N grid
-		if (N<=0 || T<=0) throw new IllegalArgumentException();
-		this.t=T;
-		this.n=N;
-		threshold = new double[t];
-		int[] openSequence = new int[n*n];
-		for (int i=0; i<n*n; i++){
-			openSequence[i]=i+1;
+		if (N <= 0 || T <= 0)
+			throw new IllegalArgumentException();
+		this.t = T;
+		this.n = N;
+		double[] threshold = new double[t];
+		int[] openSequence = new int[n * n];
+		for (int i = 0; i < n * n; i++) {
+			openSequence[i] = i + 1;
 		}
-		for (int i=0; i<t; i++){
-			Percolation test = new Percolation(n*n);
+		for (int i = 0; i < t; i++) {
+			Percolation test = new Percolation(n);
 			StdRandom.shuffle(openSequence);
-			for (int j=0; j<N*N; j++){
-				int col = openSequence[j]%n;
-				int row = (openSequence[j]-col)/n+1;
-				test.open(row,col);
-				if (test.percolates()){
-					threshold[i]=j;
+			for (int j = 0; j < n * n; j++) {
+				int col = openSequence[j] % n + 1;
+				int row = (openSequence[j] - 1) / n + 1;
+				test.open(row, col);
+				if (test.percolates()) {
+					threshold[i] = ((double) j+1) / (n * n);
 					break;
 				}
-			}			
+			}
 		}
-	}
-	
-	public double mean(){
-		// sample mean of percolation threshold
-		double sum = 0;
-		for (int i=0; i<t; i++){
-			sum += threshold[i];
-		}
-		return sum/t;		
-	}
-	
-	public double stddev(){
-		// sample standard deviation of percolation threshold
-		double mean = mean();
-		double var = 0;
-		for (int i=0; i<t; i++){
-			var += (threshold[i]-mean)*(threshold[i]-mean);			
-		}
-		return Math.sqrt(var/(t-1));
-	}
-	
-	public double confidenceLo(){
-		// low  endpoint of 95% confidence interval
-		double mean = mean();
-		double std = stddev();
-		return (mean - 1.96*std/Math.sqrt(t));
+		mean = StdStats.mean(threshold);
+		stddev = StdStats.stddev(threshold);
 	}
 
-	public double confidenceHi(){
+	public double mean() {
+		// sample mean of percolation threshold
+		return mean;
+	}
+
+	public double stddev() {
+		// sample standard deviation of percolation threshold
+		return stddev;
+	}
+
+	public double confidenceLo() {
+		// low endpoint of 95% confidence interval
+		return (mean - 1.96 * stddev / Math.sqrt(t));
+	}
+
+	public double confidenceHi() {
 		// high endpoint of 95% confidence interval
-		double mean = mean();
-		double std = stddev();
-		return (mean + 1.96*std/Math.sqrt(t));
+		return (mean + 1.96 * stddev / Math.sqrt(t));
 	}
 
 	public static void main(String[] args) {
 		
-		PercolationStats myTest = new PercolationStats(200, 20);
-		
-		System.out.println(myTest.mean());
-		System.out.println(myTest.stddev());
+		int N = Integer.parseInt(args[0]);
+		int T = Integer.parseInt(args[1]);
 
+		PercolationStats myTest = new PercolationStats(N, T);
 
+		System.out.println("mean			 = "+ myTest.mean());
+		System.out.println("stddev			 = "+ myTest.stddev());
+		System.out.println("95/% confidence interval = "+ myTest.confidenceLo() + ", "+myTest.confidenceHi());
 	}
 
 }
