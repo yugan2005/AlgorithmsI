07ef2f3c8d441cb270c3e19d6541f6deb83192a3
yugan2005 <yugan2005@gmail.com>
2015-10-03 11:56:23 -0700

    Config eclipse with github enabled

---
+++ src/Week3Quiz1/HelloWorld.java
@@ -0,0 +1,8 @@
+package Week3Quiz1;
+
+public class HelloWorld {
+	public static final void main(String[] args){
+		System.out.println("Testing of the Eclipse project setting with github enabled");
+	}
+
+}
