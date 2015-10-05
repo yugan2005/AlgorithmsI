e14c6ab9dfc1599264dc91565532508336a83bb7
yugan2005 <yugan2005@gmail.com>
2015-10-04 21:29:57 -0700

    Continue working on the FastSort method

--- src/FastCollinearPoints.java
+++ src/FastCollinearPoints.java
@@ -1,7 +1,9 @@
 import java.util.Comparator;
 
+import edu.princeton.cs.algs4.MergeX;
 import edu.princeton.cs.algs4.Quick;
 import edu.princeton.cs.algs4.ResizingArrayQueue;
+import edu.princeton.cs.algs4.ST;
 
 public class FastCollinearPoints {
 	
@@ -24,18 +26,45 @@ public class FastCollinearPoints {
 		}
 
 		lineSegmentQueue = new ResizingArrayQueue<>();
+		ST<Double,ResizingArrayQueue<Point>> tableOfSlope = new ST<>();
 		
-		for (int i=0; i<numOfPoints; i++){
+		for (int i=0; i<numOfPoints-3; i++){
 			Point point = points[i];
 			Comparator<Point> slopeComparator = point.slopeOrder();
-			Point[] otherPoints = new Point[numOfPoints-1];
-			for (int j=0; j<i; j++){
-				otherPoints[j]=points[i];
+			int numOtherPoints = numOfPoints-i-1;
+			Point[] otherPoints = new Point[numOtherPoints];
+			for (int j=0; j<numOtherPoints; j++){
+				otherPoints[j]=points[i+j+1];
 			}
-			for (int j=i; j<numOfPoints-2; j++){
-				otherPoints[j]=points[i+1];
+			MergeX.sort(otherPoints, slopeComparator);
+			int pointCounter = 1;
+			double slope = point.slopeTo(otherPoints[0]);
+			for (int j=1; j<numOtherPoints; j++){
+				if (slope == point.slopeTo(otherPoints[j])) {
+					pointCounter++;
+				}
+				else {
+					if (pointCounter>=4) {
+						if (tableOfSlope.get(slope)==null) {
+							lineSegmentQueue.enqueue(new LineSegment(point, otherPoints[j-1]));
+							ResizingArrayQueue<Point> pointWithThisSlope = new ResizingArrayQueue<>();
+							pointWithThisSlope.enqueue(point);
+							for (int k=j-pointCounter; k<j; k++){
+								pointWithThisSlope.enqueue(otherPoints[k]);
+							}
+							tableOfSlope.put(slope, pointWithThisSlope);
+						}
+						else {
+							
+						}
+						
+					}
+					
+					
+					slope = point.slopeTo(otherPoints[j]);
+					
+				}
 			}
-			
 			
 		}
 		
