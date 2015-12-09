1e6157248a282486cab970c2c6f30b2e0e9dd0dc
yugan2005 <yugan2005@gmail.com>
2015-12-08 21:55:42 -0800

    Still has a bug

--- src/Quiz5BQuiz2/Prob1.java
+++ src/Quiz5BQuiz2/Prob1.java
@@ -1,39 +1,45 @@
 package Quiz5BQuiz2;
 
 public class Prob1 {
-	private static final int  R=3;
+	private static final int R = 3;
 	private static final char startChar = 'A';
-	
-	
-	public static int[][] DFATable(String pattern){
-		if (pattern.length()==0) return null;
-		
+
+	public static int[][] DFATable(String pattern) {
+		if (pattern.length() == 0)
+			return null;
+
 		int numOfState = pattern.length();
 		int[][] result = new int[R][numOfState];
-		
-		for (int i=0; i<R; i++){
-			if (((char) startChar+i) != pattern.charAt(0)){
-				result[i][0]=0;
-			}
-			else result[i][0]=1;
+
+		for (int i = 0; i < R; i++) {
+			result[i][0] = 0;
 		}
-		
+		result[pattern.charAt(0) - startChar][0] = 1;
+
 		int xState = 0;
-		
-		for (int j=1; j<numOfState; j++){
-			for (int i=0; i<R; i++){
+
+		for (int j = 1; j < numOfState; j++) {
+			for (int i = 0; i < R; i++) {
 				result[i][j] = result[i][xState];
 			}
-			result[pattern.charAt(j)-startChar][j]=j+1;
-			xState = result[pattern.charAt(j)][xState];
+			result[pattern.charAt(j) - startChar][j] = j + 1;
+			xState = result[pattern.charAt(j) - startChar][xState];
 		}
 		return result;
-		
+
 	}
 
 	public static void main(String[] args) {
-		
-		int[][] output = DFATable("BBCBBCBA".trim().split("\\s+").ap)
+
+		int[][] output = DFATable("BBCBBCBA");
+
+		for (int i = 0; i < output.length; i++) {
+			StringBuilder sb = new StringBuilder();
+			for (int j = 0; j < output[i].length; j++) {
+				sb.append(" " + output[i][j]);
+			}
+			System.out.println(sb.toString().trim());
+		}
 
 	}
 
